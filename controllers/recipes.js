091d273b0a925d668638b31064c361cab70c21fd
gabriel <gabriel@pop-os.localdomain>
2020-07-05 13:39:48 -0300

    Formulario de criação part 2

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+const data = require('../data.json')
 const recipes = require("../data")
 exports.index = function(req, res){
   const recipe = recipes ;
@@ -7,17 +9,45 @@ exports.create = function(req, res){
   return res.render('admin/recipes/create')
 }
 exports.show = function(req, res){
-  const recipe = recipes ;
-  const recipeIndex = req.params.index
-  console.log(req.params.index)
-  
-  return res.render('admin/recipes/recipe', {items: recipe[recipeIndex]})    
+  const { id } = req.params
+ // console.log(data.recipes.id)
+  const foundRecipe = data.recipes.find(function(recipe){
+    return recipe.id == id
+  })
+
+  if (!foundRecipe) return res.send('Receita não encontrada')
+
+  const recipe = {
+    ...foundRecipe
+  }
+  return res.render('admin/recipes/recipe', { items: recipe })
 }
 exports.edit = function(req, res){
 return res.send('/admin/recipes/:id/edit')
 }
 exports.post = function(req, res){
-  return res.send(req.body)
+  const keys = Object.keys(req.body)
+
+  let {image, title, author, ingredients, preparation, information} = req.body
+
+  const id = Number(data.recipes.length + 1)
+
+  data.recipes.push({
+    id,
+    image,
+    title,
+    author,
+    ingredients,
+    preparation,
+    information
+  })
+
+  fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err){
+    if(err) return res.send('write file error')
+
+    return res.redirect(`recipes/${id}`)
+  })
+ // return res.send(req.body)
 }
 exports.put = function(req, res){
   return res.send('put')

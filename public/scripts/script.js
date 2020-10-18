5a5b12711c68d3160e348ef4d50bcc47dc7f1490
gabriel <gabriel@pop-os.localdomain>
2020-10-18 15:22:46 -0300

    refactory

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -87,3 +87,4 @@ if (pagination) {
     createPaginaton(pagination)
 
 }
+
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ const foodfy = require('./app/controllers/foodfy')
 const recipes = require('./app/controllers/recipes')
 const chefs = require('./app/controllers/chefs')
 
+const multer = require('./app/middleware/multer')
+
 //rotas do foodfy
 
 routes.get("/", foodfy.index)
@@ -36,8 +38,8 @@ routes.get("/admin/recipes/create", recipes.create); // Mostrar formulário de n
 routes.get("/admin/recipes/:id", recipes.show); // Exibir detalhes de uma receita
 routes.get("/admin/recipes/:id/edit", recipes.edit); // Mostrar formulário de edição de receita
 
-routes.post("/admin/recipes", recipes.post); // Cadastrar nova receita
-routes.put("/admin/recipes", recipes.put); // Editar uma receita
+routes.post("/admin/recipes",multer.array("photos", 6), recipes.post); // Cadastrar nova receita
+routes.put("/admin/recipes",multer.array("photos", 6), recipes.put); // Editar uma receita
 routes.delete("/admin/recipes", recipes.delete); // Deletar uma receita
 
 module.exports = routes
